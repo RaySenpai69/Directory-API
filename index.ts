316fa1d53ca383fdb38658ab39472d4faa143a7b
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-01 18:54:46 +0530

    Directory API 😁

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { readdirSync } from "fs";
+
+import express from "express";
+
+const app: express.Application = express();
+
+const port: number = Number(process.env.PORT) || 8080;
+
+let dir = process.cwd();
+
+app.get("/", (req, res) => {
+  res.send("Hello Oni-Chan");
+});
+app.get("/random", (req, res) => {
+  const directory = "/images/";
+  const files = readdirSync(`.${directory}`);
+  let randomFile = files[Math.floor(Math.random() * files.length)];
+  res.sendFile(dir + `${directory}${randomFile}`);
+});
+app.get("*", (req, res) => {
+  res.redirect("/random");
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
+});
