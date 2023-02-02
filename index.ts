a724b87693a7703f0337282ea06514026b4466e6
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-02 14:30:29 +0530

    Sup

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import { readdirSync } from "fs";
-
 import express from "express";
-
 const app = express();
-
 const port: number = Number(process.env.PORT) || 8080;
-
-let dir = process.cwd();
-
+const _dirname = process.cwd();
 app.get("/", (req, res) => {
   res.send("Hello Oni-Chan");
 });
@@ -15,7 +10,7 @@ app.get("/random", (req, res) => {
   const directory = "/images/";
   const files = readdirSync(`.${directory}`);
   let randomFile = files[Math.floor(Math.random() * files.length)];
-  res.sendFile(dir + `${directory}${randomFile}`);
+  res.sendFile(_dirname + `${directory}${randomFile}`);
 });
 app.get("*", (req, res) => {
   res.redirect("/random");
