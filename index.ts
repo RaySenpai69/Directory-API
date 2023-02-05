5ebff3cd2c1556e47876ae2c5869ca66a85f4066
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-05 19:21:47 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,20 +4,20 @@ const fastify = Fastify();
 const port: number = Number(process.env.PORT) || 8080;
 const _dirname = process.cwd();
 
-fastify.get('/', function(request, reply){
-reply.send('Hello Oni-chan')
-})
+fastify.get("/", function (request, reply) {
+  reply.send("Hello Oni-chan");
+});
 
-fastify.get('/random', function (request, reply) {
+fastify.get("/random", function (request, reply) {
   const directory = "/images/";
   const files = readdirSync(_dirname + directory);
   let randomFile = files[Math.floor(Math.random() * files.length)];
-  reply.header('Content-Type', 'image/jpeg');
+  reply.header("Content-Type", "image/jpeg");
   reply.send(createReadStream(_dirname + `${directory}${randomFile}`));
 });
-fastify.get('*', function (request, reply) {
-  reply.redirect('/random')
-})
+fastify.get("*", function (request, reply) {
+  reply.redirect("/random");
+});
 fastify.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
